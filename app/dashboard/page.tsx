2cd20c4282fba9c2e4c6c6ab985fafbfa260ba48
Jayce <abenalesjc11@gmail.com>
2024-01-17 00:56:12 +0800

    Finished Chapter 11

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,6 @@ import {
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -19,7 +18,7 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           {/* @ts-ignore */}
-         <CardWrapper/>
+          <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
