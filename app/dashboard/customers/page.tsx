01f567b8d0b8b4159ad42dc4786dc511ff553ebe
Jayce <abenalesjc11@gmail.com>
2024-01-18 19:49:35 +0800

    Adding Breadcrumbs

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 
 const Page = () => {
-  return <div>Customers Page</div>;
+  return (
+    <div className="w-full">
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Customers', href: '/dashboard/customers', active: true },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Page;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,13 +8,14 @@ import {
   RevenueChartSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import Breadcrumbs from '../ui/invoices/breadcrumbs';
 
 export default async function Page() {
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
+      <Breadcrumbs
+        breadcrumbs={[{ label: 'Dashboard', href: '/dashboard', active: true }]}
+      />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           {/* @ts-ignore */}
